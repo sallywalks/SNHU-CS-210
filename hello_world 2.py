eb3c590b4822cd56e94ddbee7f42d3ebe622b689
JJ <joolney21@gmail.com>
2025-08-05 04:00:34 -0400

    hello_world
    hello world in c++ and python

---
+++ hello_world 2.py
@@ -0,0 +1,6 @@
+#JoAnn Olney
+#07/01/2025
+#CS-210 assignment one mod 1-5
+
+#print Hello, World!
+print("Hello, World!")
---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+//JoAnn Olney
+//07/01/2025
+//CS-210 assignment 1 mod 1-5
+
+//include iostream for input/output
+#include <iostream>
+
+//the entry point of the program 
+int main()
+{
+//print Hello, World!
+  std::cout << "Hello, World!\n";
+}
